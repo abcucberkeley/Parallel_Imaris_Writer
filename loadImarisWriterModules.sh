598c145774ad3bda869f52fa06e31a2f3a871481
MattHazard <mrm.mue@gmail.com>
2024-05-02 23:35:16 -0700

    Moved c-tiff to cpp-tiff and major repo cleanup in preparation for the self-contained version

--- loadImarisWriterModules.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-bash
-module load blosc/2.0.4
-module load cJSON/1.7.15
-module load ImarisWriter/1.0.0
-module load hdf5/1.12.1
-module load gcc/12.2.0
-#module load gcc/8.3.0
--- mallocDynamic.h
+++
@@ -1,7 +0,0 @@
-#ifndef MALLOCDYNAMIC_H
-#define MALLOCDYNAMIC_H
-#include <stdint.h>
-
-void* mallocDynamic(uint64_t x, uint64_t bits);
-
-#endif // MALLOCDYNAMIC_H
